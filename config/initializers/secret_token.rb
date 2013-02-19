408f7afb43923abd93bc04ae8ad4aa1d71202495
Erlendur <erlendure@gmail.com>
2013-02-19 14:11:02 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offerstore::Application.config.secret_token = 'dfc91a34725d3f4b2f515f444cfc61006e2226966464c816a88c51797f4b0c9cec362468d6ae138ee507ccdaa63391a4a9b17a521472df9fd4092d77973d571d'
